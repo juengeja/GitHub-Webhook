78e610e1bee7736d591eca0deec43f1919ca0b91
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2023-03-21 11:39:53 +0100

    Update listener.js

--- listener.js
+++ listener.js
@@ -1,48 +1,55 @@
-const PORT = process.env.GITHUB_WEBHOOK_PORT || 3311;
-const secret = process.env.GITHUB_WEBHOOK_SECRET || '';
-const { createServer } = require('http');
+const express = require('express');
+const { createHmac } = require('crypto');
+const { exec } = require('child_process');
 
-const server = createServer((req, res) => {
-    if('POST' === req.method){
-        console.log("Got a post request!")
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        console.log(`Body: ${body}`);
-        req.on('end', () => {
-            if('' !== secret){
-                const { createHmac } = require('crypto');
-                let signature = createHmac('sha1', secret).update(body).digest('hex');
-                if(req.headers['x-hub-signature'] !== `sha1=${signature}`){
-                    console.log('Signature error');
-                    res.statusCode = 403;
-                    res.end();
-                    return;
-                }
-            }else{
-                console.log("No secret provided!")
-            }
-            try{
-                body = JSON.parse(decodeURIComponent(body).replace(/^payload=/, ''));
-            }catch(e){
-                console.log(e);
-            }
-        })
-        console.log(`body.ref: ${body.ref}`);
-        if('object' === typeof body){
-            if('refs/heads/main' === body.ref){
-                const { exec } = require('child_process');
-                const command = `cd ../${body.repository.name} && git pull origin main`;
-                console.log("Executing command")
-                exec(command, (error, stdout, stderr) => {
+const app = express();
+const port = process.env.PORT || 3311;
+const secret = process.env.GITHUB_WEBHOOK_SECRET;
 
-                })
-            }
-        }
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+  const signature = req.headers['x-hub-signature'];
+  const event = req.headers['x-github-event'];
+  const id = req.headers['x-github-delivery'];
+
+  if (!signature) {
+    console.log('Signature missing');
+    return res.status(400).send('Signature missing');
+  }
+
+  if (!event || !id) {
+    console.log('Event or ID missing');
+    return res.status(400).send('Event or ID missing');
+  }
+
+  if (secret) {
+    const hmac = createHmac('sha1', secret);
+    const digest = Buffer.from('sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex'), 'utf8');
+    const checksum = Buffer.from(signature, 'utf8');
+    if (!crypto.timingSafeEqual(digest, checksum)) {
+      console.log('Signature mismatch');
+      return res.status(400).send('Signature mismatch');
     }
-})
+  }
 
-server.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
-})
+  if (event === 'push' && req.body.ref === 'refs/heads/main') {
+    const command = `cd ../${req.body.repository.name} && git pull origin main`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`Error executing command: ${error}`);
+        return res.status(500).send(`Error executing command: ${error}`);
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+      return res.status(200).send('Webhook received');
+    });
+  } else {
+    console.log(`Event ${event} not supported`);
+    return res.status(400).send(`Event ${event} not supported`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
